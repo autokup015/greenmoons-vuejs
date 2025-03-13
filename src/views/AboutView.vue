789b1a5dd3d4030cf5c662cac1e6ede244f06a6d
Auto <chonlatee@dosetech.co>
2025-03-13 20:45:10 +0700

    feat: connect api done

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,81 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <h1>Hello about page : {{ $route.params.id }}</h1>
+
+    <div v-if="!detailTodo">
+      <h1>No data</h1>
+    </div>
+
+    <div class="card-detail" v-else>
+      <h2>{{ detailTodo?.id }} : {{ detailTodo.description }}</h2>
+
+      <h2>
+        Status :
+        <span :style="`color : ${detailTodo.completed ? 'green' : 'red'}`">
+          {{ detailTodo.completed ? 'Done' : 'Not Done' }}
+        </span>
+      </h2>
+
+      <h2>Create : {{ dayjs(detailTodo.created_at).format('DD / MM / YYYY') }}</h2>
+
+      <h2>Update : {{ dayjs(detailTodo.updated_at).format('DD / MM / YYYY') }}</h2>
+    </div>
+
+    <button @click="router.push('/')">Go Home pages</button>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/counter'
+import axios from 'axios'
+import dayjs from 'dayjs'
+import { onMounted, ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+
+// --------------------------- Type ---------------------------
+
+type TTodoData = {
+  completed: boolean
+  created_at: string
+  description: string
+  id: number
+  updated_at: string
+}
+
+type TTodoDetailResponse = {
+  data: TTodoData
+  success: boolean
+}
+
+// --------------------------- State ---------------------------
+
+const { ENDPOINT } = useCounterStore()
+
+const { params } = useRoute()
+
+const router = useRouter()
+
+const detailTodo = ref<TTodoData | null>(null)
+
+// --------------------------- Same like Use effect ---------------------------
+
+onMounted(() => {
+  fetchDetailTodoList()
+})
+
+// --------------------------- Function ---------------------------
+
+const fetchDetailTodoList = async () => {
+  const { data } = await axios.get<TTodoDetailResponse>(`${ENDPOINT}/api/todos/${params.id}`)
+
+  detailTodo.value = data.data
+}
+</script>
+
+<style scoped>
+.card-detail {
+  padding: 15px;
+  border: 1px solid white;
+  border-radius: 10px;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,231 @@
+<template>
+  <div>
+    <h1>Home pages</h1>
+
+    <hr />
+
+    <!-- ! Create todo list -->
+
+    <div>
+      <h1>Create todo list</h1>
+
+      <div class="flex-box">
+        <input type="text" v-model="inputValue" @keydown.enter="createTodoList" />
+
+        <button @click="createTodoList">Create</button>
+      </div>
+    </div>
+
+    <!-- ! Loading -->
+
+    <div class="box-loading-blank" v-if="isLoading">
+      <h1>Loading ...</h1>
+    </div>
+
+    <!-- ! No data -->
+
+    <div class="box-loading-blank" v-if="!isLoading && !stateTodo.data.length">
+      <h1>No data Naja nong nong !</h1>
+    </div>
+
+    <!-- ! Render item -->
+
+    <div class="box-card">
+      <div class="card" v-for="(item, index) in stateTodo.data" :key="item.id">
+        <!-- ! No edit case -->
+        <div class="data" v-if="selectedData?.id !== item.id">
+          <p>{{ index + 1 }} : {{ item.description }}</p>
+
+          <div class="flex-box">
+            <p :style="`color : ${item.completed ? 'green' : 'red'}`">
+              {{ item.completed ? 'Done' : 'Not Done' }}
+            </p>
+
+            <button @click="goDetail(item.id)">Go Detail</button>
+
+            <button @click="onEditData(item)">Edit</button>
+
+            <button @click="deleteTodoList(item.id)">Delete</button>
+          </div>
+        </div>
+
+        <!-- ! Edit case -->
+        <div class="data" v-else>
+          <div class="flex-box flex-1">
+            <p>{{ index + 1 }} :</p>
+            <input class="input-text" type="text" v-model="selectedData.description" />
+            <input type="checkbox" v-model="selectedData.completed" />
+          </div>
+
+          <div class="flex-box">
+            <div class="flex-box">
+              <button @click="cancelSelected">Cancel</button>
+              <button @click="updateTodoList">Save</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+// --------------------------- Type ---------------------------
+
+type TTodoData = {
+  completed: boolean
+  created_at: string
+  description: string
+  id: number
+  updated_at: string
+}
+
+type TTodoState = {
+  data: Array<TTodoData>
+}
+
+type TTodoResponse = {
+  data: Array<TTodoData>
+  success: boolean
+}
+
+type TUpdateTodoResponse = {
+  description: string
+  completed: boolean
+}
+
+type TDeleteResponse = {
+  data: number
+  success: boolean
+}
+
+import { useCounterStore } from '@/stores/counter'
+// --------------------------- Import ---------------------------
+
+import axios from 'axios'
+import { onMounted, reactive, ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+// --------------------------- State ---------------------------
+
+const router = useRouter()
+
+const { ENDPOINT } = useCounterStore()
+
+const isLoading = ref(false)
+
+const stateTodo = reactive<TTodoState>({
+  data: [],
+})
+
+const selectedData = ref<TTodoData | null>(null)
+
+const inputValue = ref('')
+
+// --------------------------- Same like Use Effect ---------------------------
+
+onMounted(() => {
+  fetchTodoList()
+})
+
+// --------------------------- Api ---------------------------
+
+const fetchTodoList = async () => {
+  isLoading.value = true
+
+  const { data } = await axios.get<TTodoResponse>(`${ENDPOINT}/api/todos`)
+
+  stateTodo.data = data.data
+
+  isLoading.value = false
+}
+
+const createTodoList = async () => {
+  const paylaod = {
+    description: inputValue.value || 'มึงอย่าปล่อยว่างอิสัส',
+  }
+
+  await axios.post(`${ENDPOINT}/api/todos`, paylaod)
+
+  inputValue.value = ''
+  fetchTodoList()
+}
+
+const updateTodoList = async () => {
+  if (!selectedData.value) {
+    return
+  }
+
+  const payload: TUpdateTodoResponse = {
+    description: selectedData.value.description || 'เอาอีกละ มึงอย่าปล่อยว่างอิสัส',
+    completed: selectedData.value.completed,
+  }
+
+  await axios.put(`${ENDPOINT}/api/todos/${selectedData.value.id}`, payload)
+
+  selectedData.value = null
+
+  fetchTodoList()
+}
+
+const deleteTodoList = async (id: number) => {
+  const { data } = await axios.delete<TDeleteResponse>(`${ENDPOINT}/api/todos/${id}`)
+
+  stateTodo.data = stateTodo.data.filter((item) => item.id !== data.data)
+}
+
+// --------------------------- Function ---------------------------
+
+const onEditData = (data: TTodoData) => {
+  selectedData.value = { ...data }
+}
+
+const cancelSelected = () => {
+  selectedData.value = null
+}
+
+const goDetail = (id: number) => {
+  router.push(`/about/${id}`)
+}
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped>
+.box-card {
+  max-width: 800px;
+  margin: auto;
+
+  .card {
+    padding: 15px;
+    border: 1px solid white;
+    border-radius: 10px;
+    margin: 10px 0px;
+
+    .data {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+  }
+}
+
+.flex-box {
+  display: flex;
+  gap: 10px;
+
+  .input-text {
+    width: 100%;
+    max-width: 300px;
+  }
+}
+
+.flex-1 {
+  flex-grow: 1;
+}
+
+/* --------------------------- Loading --------------------------- */
+
+.box-loading-blank {
+  padding: 200px;
+  text-align: center;
+}
+</style>
